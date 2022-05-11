1f454a92c698d4041dab8bb6f709d2fa068c19ff
Julio <julioaranabr@yahoo.com.br>
2022-05-10 21:22:40 -0300

    controller  Product

---
+++ src/database/controllers/Product.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import ProductService from '../services/Product';
+
+class ProductController {
+  static async addProduct(req: Request, res: Response) {
+    try {
+      const { body } = req;
+      const product = await ProductService.addProduct(body);
+      return res.status(201).json(product);
+    } catch (error) {
+      return res.status(500).json({ message: 'Internal Server Error '});
+    }
+  }
+}
+
+export default ProductController;
