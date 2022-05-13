f4d90901c027e2b47c53eb7b4d98a28c58e2c291
Julio <julioaranabr@yahoo.com.br>
2022-05-12 23:22:46 -0300

    refatored validations insert products

--- src/database/middlewares/Products/validateProduct.ts
+++ src/database/middlewares/Products/validateProduct.ts
@@ -1,26 +1,13 @@
 import { Request, Response, NextFunction } from "express";
-import { verify } from 'jsonwebtoken';
-import * as fs from 'fs';
-import { UserDataI } from "../../../../src/domain/domain";
-const key = fs.readFileSync('jwt.evation.key', 'utf-8');
 
 export default class ValidateProduct {
   static validateProduct(req: Request, res: Response, next: NextFunction) {
    try {
-    const { authorization } = req.headers;
     const { name, price } = req.body;
     
     if(!name || !price) {
       return res.status(401).json({ message: 'All fields must be filled' })
     }
-    if(!authorization) {
-      return res.status(401).json({ message: 'Token not found' })
-    }
-
-    const decodedToken = verify(authorization,key);
-    const { role } = decodedToken as UserDataI;
-
-    if(role !== 'admin') return res.status(401).json({ message: 'User Unauthorized' })
 
     next();
    } catch (error) {
