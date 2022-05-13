0f365f1721aba60c84a2d7ca3a43f8f38f752bf6
Julio <julioaranabr@yahoo.com.br>
2022-05-12 23:24:30 -0300

    added validation token in route products

--- src/routes/ProductRoute.ts
+++ src/routes/ProductRoute.ts
@@ -2,11 +2,13 @@ import * as express from 'express';
 import { upload } from '../../src/helper/upload';
 import ProductController from '../../src/database/controllers/Product';
 import ValidateProduct from '../../src/database/middlewares/Products/validateProduct';
+import { ValidateToken } from '../../src/database/middlewares/Token/validateToken';
 
 const ProductRouter = express.Router();
 
 ProductRouter
   .use(upload.single('image'))
+  .use(ValidateToken.checkToken)
   .use(ValidateProduct.validateProduct)
   .post('/',ProductController.addProduct)
 
