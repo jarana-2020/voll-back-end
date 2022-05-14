ae174836f0ca260402a06432bfdbccef8af7d245
Julio <julioaranabr@yahoo.com.br>
2022-05-14 19:10:25 -0300

    added method getAll in controller product

--- src/database/controllers/Product.ts
+++ src/database/controllers/Product.ts
@@ -13,6 +13,16 @@ class ProductController {
       return res.status(500).json({ message: 'Internal Server Error '});
     }
   }
+
+  static async getAll(req: Request, res: Response) {
+    try {
+      const product = await ProductService.getAll();
+      
+      return res.status(201).json(product);
+    } catch (error) {
+      return res.status(500).json({ message: 'Internal Server Error '});
+    }
+  }
 }
 
 export default ProductController;
